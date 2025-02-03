2d09ba8ed944ec437d940bcaa3f2b7441fc65fc6
statox <me@statox.fr>
2025-02-03 19:48:39 +0100

    Config - Update config file with templates for existing sensors

--- libraries/Statox_Config/config.sample.h
+++ libraries/Statox_Config/config.sample.h
@@ -1,5 +1,10 @@
 #define CONFIG_API_HOSTNAME "api.statox.fr"
-#define CONFIG_SLEEP_SECONDS 60 * 10
+
+// Only one DHT sensor by board is supported
+// (which is fine I don't expect to have boards with 2 DHTs)
+#define DHTPIN A1     // Pin used to read the DHT sensor
+#define DHTTYPE DHT11 // DHT11 or DHT22
+
 #define CONFIG_SENSOR_NAME "dev-sensor"
 
 // One of these must be defined
@@ -11,7 +16,23 @@
 // them is used to monitor inside of the enclosure
 #define HAS_INTERNAL_SENSOR
 
-// Only one DHT sensor by board is supported
-// (which is fine I don't expect to have boards with 2 DHTs)
-#define DHTPIN A1     // Pin used to read the DHT sensor
-#define DHTTYPE DHT11 // DHT11 or DHT22
+/* SDB */
+// #define CONFIG_SENSOR_NAME "sdb"
+// #define MAIN_SENSOR_SHT
+// #define HAS_INTERNAL_SENSOR
+// #define DHTTYPE DHT22 // DHT11 or DHT22
+
+/* Chambre */
+// #define CONFIG_SENSOR_NAME "chambre"
+// #define MAIN_SENSOR_DHT
+// #define DHTTYPE DHT22 // DHT11 or DHT22
+
+/* Salon */
+// #define CONFIG_SENSOR_NAME "salon"
+// #define MAIN_SENSOR_BME
+
+/* Jardiniere */
+// #define CONFIG_SENSOR_NAME "jardiniere"
+// #define MAIN_SENSOR_SHT
+// #define HAS_INTERNAL_SENSOR
+// #define DHTTYPE DHT11 // DHT11 or DHT22
